3f4fb75646d0b271eababb63aa7f9fc03cd260e7
louiseaubet <55659486+louiseaubet@users.noreply.github.com>
2020-12-16 11:07:56 +0100

    Add files via upload

--- sensitivity_analysis.R
+++ sensitivity_analysis.R
@@ -1,21 +1,24 @@
 
-library(dplyr)
-library(reshape2)
-library(ggplot2)
 library(sensitivitymv)
 
 # Load data
-
-filename <- "../data/P3/X_shcementfloor.csv" #X_S_satislife.csv"
+filename <- "../data_sensitivity_analysis/X_paper_S_shcementfloor.csv"
 file.exists(filename)
 
-y <- read.table(filename, sep=",", header= TRUE)
-head(y)
-
 # Sensitivity analysis
-  
-senmv(y, gamma=1, method=NULL, inner=0, trim=2.5, lambda=1/2, tau=0, TonT=FALSE)
+X <- read.table(filename, sep=",", header= TRUE)
+senmw(X, gamma=3.25, method = NULL, inner = 0, trim = 2.5, lambda = 1/2,
+      tau = 0)
 
-  # Amplification of sensitivity analysis
+# Amplification of sensitivity analysis
+delta=numeric(0)
+for(i in 1:1000)
+{delta[i]<-(amplify(3.25, seq(3.30, 5, length.out = 1000)[i]))}
 
-#amplify(gamma=2, 3)
+lambda<- seq(3.30, 5, length.out = 1000)
+
+g = data.frame(lambda, delta)
+
+plot(lambda, delta)
+
+write.csv(g, "../data_sensitivity_analysis/amplification/amp_paper_S_shcementfloor.csv", row.names = FALSE)
